151e1d7dc19daa0091c05bcf3cf071eb32b6d720
Hoyeon Luke Jang <hjang55@uwo.ca>
2024-06-02 22:49:51 -0400

    started applying parallax effects and started building landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Parallax pages = {15}>
+
+        <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundColor: '#80404E' }}>
+
+          <h1>DAMN.</h1>
+
+        </ParallaxLayer>
+
+        
+        
+
+
+      </Parallax>
+
+
+    </div>
+
+
   )
 }
 
