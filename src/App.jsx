94cc5d2dd81a810ad240e3d5998a0f7ad7b6ceea
Hoyeon Luke Jang <hjang55@uwo.ca>
2024-06-05 18:24:09 -0400

    Working on page 2 BLOOD.

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         {/* "DAMN." text in the center of the first page */}
         <ParallaxLayer
           offset={0}
-          speed={0.1}
+          speed={1}
           style={{ ...alignCenter, justifyContent: 'center', zIndex: 3 }}
         >
           <h1 style={{ color: "#FF002A" }}>DAMN.</h1>
@@ -23,13 +23,12 @@ function App() {
         {/* Kendrick's picture positioned at the bottom of page two */}
         <ParallaxLayer
           sticky={{ start: 0, end: 1 }}
-          speed={1}
           style={{
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'flex-end',
             height: '100vh',
-            zIndex: -1
+            zIndex: -2
           }}
         >
           <img src='./images/KendrickPage2.png' alt="Kendrick Lamar" style={{ width: '50%' }} />
@@ -37,41 +36,15 @@ function App() {
 
         {/* BLOOD text with a different speed */}
         <ParallaxLayer
-          speed={0.5}
-          style={{ ...alignCenter, justifyContent: 'center', zIndex: -1 }}
+          sticky={{ start: 0, end: 1.02 }}
+          style={{ display: 'flex', justifyContent: 'center', zIndex: -1 }}
         >
-          <h1 style={{ color: "#FF002A" }}>BLOOD.</h1>
+          <h1 style={{ bottom: '80vh', color: "#FF002A" }}>BLOOD.</h1>
         </ParallaxLayer>
 
         {/* Additional layers for demonstrating speed differences */}
         {/* Fast-moving layer */}
-        <ParallaxLayer
-          offset={1}
-          speed={2}
-          style={{
-            ...alignCenter,
-            justifyContent: 'center',
-            zIndex: 4,
-            background: 'rgba(255, 255, 255, 0.5)',
-          }}
-        >
-          <h1 style={{ color: "#000" }}>Fast Moving Text</h1>
-        </ParallaxLayer>
-
-        {/* Slow-moving layer */}
-        <ParallaxLayer
-          offset={2}
-          speed={0.2}
-          style={{
-            ...alignCenter,
-            justifyContent: 'center',
-            zIndex: 4,
-            background: 'rgba(0, 0, 0, 0.5)',
-          }}
-        >
-          <h1 style={{ color: "#FFF" }}>Slow Moving Text</h1>
-        </ParallaxLayer>
-
+        
       </Parallax>
     </div>
   );
