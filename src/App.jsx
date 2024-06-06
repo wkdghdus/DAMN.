ba616291baf3fc7967cea425a38f471b94054848
Hoyeon Luke Jang <hjang55@uwo.ca>
2024-06-06 18:49:46 -0400

    working on page 3

--- src/App.jsx
+++ src/App.jsx
@@ -9,41 +9,45 @@ function App() {
     <div>
       <Parallax pages={15} horizontal={true}>
         {/* Background color of the first page */}
-        <ParallaxLayer offset={0} speed={0} style={{ background: '#80404E', zIndex: 1 }} />
+        <ParallaxLayer offset={0} speed={0} style={{ background: '#80404E', zIndex: 4 }} />
 
         {/* "DAMN." text in the center of the first page */}
         <ParallaxLayer
           offset={0}
           speed={1}
-          style={{ ...alignCenter, justifyContent: 'center', zIndex: 3 }}
+          style={{ ...alignCenter, justifyContent: 'center', zIndex: 5 }}
         >
           <h1 style={{ color: "#FF002A" }}>DAMN.</h1>
         </ParallaxLayer>
 
+        {/* page two bakground */}
+        <ParallaxLayer offset={1} speed={0} style={{ background: '#CFD3E5', zIndex: 0 }} />
+
         {/* Kendrick's picture positioned at the bottom of page two */}
         <ParallaxLayer
-          sticky={{ start: 0, end: 1 }}
+          // sticky={{ start: 0, end: 1 }}
+          offset={1}
           style={{
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'flex-end',
             height: '100vh',
-            zIndex: -2
+            zIndex: 2
           }}
         >
           <img src='./images/KendrickPage2.png' alt="Kendrick Lamar" style={{ width: '50%' }} />
         </ParallaxLayer>
+        
 
         {/* BLOOD text with a different speed */}
         <ParallaxLayer
-          sticky={{ start: 0, end: 1.02 }}
-          style={{ display: 'flex', justifyContent: 'center', zIndex: -1 }}
+          offset={1.02}
+          speed={0}
+          style={{ display: 'flex', justifyContent: 'center', zIndex: 3 }}
         >
           <h1 style={{ bottom: '80vh', color: "#FF002A" }}>BLOOD.</h1>
         </ParallaxLayer>
 
-        {/* Additional layers for demonstrating speed differences */}
-        {/* Fast-moving layer */}
         
       </Parallax>
     </div>
